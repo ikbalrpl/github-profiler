49fd91f1c52222e459c20496a62307a24692250b
ikbalrpl <ikbal.rpl80@gmail.com>
2018-02-12 19:18:18 +0700

    Finish interface in terminal/console

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 // import profile from './profile.js'
 let profile = require('./profile.js')
-console.log(profile)
+// console.log(profile)
+// let profiles = ['ikbalrpl','rizafahmi','yofri']
+// profiles.map(user => {
+//   return profile.get(user)
+// })
+profiles = process.argv.slice(2)
 
-profile.get('ikbalrpl')
-profile.get('rizafahmi')
-profile.get('yofri')
+profiles.map(profile.get)
 
+// profile.get('ikbalrpl')
+// profile.get('rizafahmi')
+// profile.get('yofri')
 
-
+// let array = [1,2,3]
+//
+// let newArray = array.map(item => {
+//   return item*2
+// })
+// console.log(newArray)
 // function functionName(param) {
 //   console.log('fungsi ini dipanggil ' + param)
 // }
